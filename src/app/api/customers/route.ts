8f5406dfd307104fd5b80266ceca8e7415f6c99d
Elitex07 <jain.yashu2211@gmail.com>
2025-04-24 02:33:56 +0530

    feat: more fields to the table

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -14,5 +14,16 @@ export async function POST(req: Request) {
     const post = await prisma.customer.create({
         data: { name, email, clerkId, panNumber, status},
     });
-    return NextResponse.json(post);
+
+    const allowedOrigins = ['http://localhost:3000', 'https://main-website-three-tau.vercel.app'];
+    const origin = req.headers.get('origin') || '';
+    
+    return NextResponse.json(post, {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : '',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        }
+    });
 }
